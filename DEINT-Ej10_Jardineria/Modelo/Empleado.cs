f2745570166b153fe888f2330172e10d9296561a
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2022-11-24 14:41:36 +0100

    V1

--- DEINT-Ej10_Jardineria/Modelo/Empleado.cs
+++ DEINT-Ej10_Jardineria/Modelo/Empleado.cs
@@ -8,6 +8,38 @@ namespace DEINT_Ej10_Jardineria.Modelo
 {
     public class Empleado
     {
+        public int codigo_empleado { get; set; }
+        public string nombre { get; set; }
+        public string apellido1 { get; set; }
+        public string apellido2 { get; set; }
+        public string extension { get; set; }
+        public string email { get; set; }
+        public Oficina oficina { get; set; }
+        public Empleado jefe { get; set; }
+        public string puesto { get; set; }
 
+        public Empleado(int codigo_empleado, string nombre, string apellido1, string apellido2, string extension, string email, Oficina oficina, Empleado jefe, string puesto)
+        {
+            this.codigo_empleado = codigo_empleado;
+            this.nombre = nombre;
+            this.apellido1 = apellido1;
+            this.apellido2 = apellido2;
+            this.extension = extension;
+            this.email = email;
+            this.oficina = oficina;
+            this.jefe = jefe;
+            this.puesto = puesto;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Empleado empleado &&
+                   codigo_empleado == empleado.codigo_empleado;
+        }
+
+        public override int GetHashCode()
+        {
+            return 1182746450 + codigo_empleado.GetHashCode();
+        }
     }
 }
--- DEINT-Ej10_Jardineria/Modelo/Oficina.cs
+++ DEINT-Ej10_Jardineria/Modelo/Oficina.cs
@@ -8,6 +8,37 @@ namespace DEINT_Ej10_Jardineria.Modelo
 {
     public class Oficina
     {
+        public int codigo_oficina { get; set; }
+        public string ciudad { get; set; }
+        public string pais { get; set; }
+        public string region { get; set; }
+        public string codigo_postal { get; set; }
+        public string telefono { get; set; }
+        public string linea_direccion1 { get; set; }
+        public string linea_direccion2 { get; set; }
+
+        public Oficina(int codigo_oficina, string ciudad, string pais, string region, string codigo_postal, string telefono, string linea_direccion1, string linea_direccion2)
+        {
+            this.codigo_oficina = codigo_oficina;
+            this.ciudad = ciudad;
+            this.pais = pais;
+            this.region = region;
+            this.codigo_postal = codigo_postal;
+            this.telefono = telefono;
+            this.linea_direccion1 = linea_direccion1;
+            this.linea_direccion2 = linea_direccion2;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Oficina oficina &&
+                   codigo_oficina == oficina.codigo_oficina;
+        }
+
+        public override int GetHashCode()
+        {
+            return 957680844 + codigo_oficina.GetHashCode();
+        }
 
     }
 }
